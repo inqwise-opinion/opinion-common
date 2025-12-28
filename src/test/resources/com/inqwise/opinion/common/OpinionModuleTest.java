8d5b93a8ac90cad9ed4182eec605882d514563d6
alex misyuk <glassfox@gmail.com>
2025-12-28 08:59:21 +0200

    OpinionModuleTest

---
+++ src/test/resources/com/inqwise/opinion/common/OpinionModuleTest.java
@@ -0,0 +1,36 @@
+package com.inqwise.opinion.common;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.inqwise.opinion.account.MainVerticleTest;
+
+import io.vertx.core.Vertx;
+import io.vertx.junit5.VertxExtension;
+
+@ExtendWith(VertxExtension.class)
+class OpinionModuleTest {
+	
+private static final Logger logger = LogManager.getLogger(OpinionModuleTest.class);
+	
+	@BeforeEach
+	void setUp(Vertx vertx) throws Exception {
+	}
+	
+	@Test
+	void testOpinionModule(Vertx vertx) {
+		logger.debug("testOpinionModule - start");
+		Guice.createInjector(List.of(new OpinionModule(vertx) {
+		})).injectMembers(this);
+	}
+
+}
