d56b0d58bd0ee89b4e39d49edc25a610b197a13f
alex misyuk <glassfox@gmail.com>
2026-01-07 10:28:14 +0200

    extend constructor with config

--- src/main/java/com/inqwise/opinion/common/OpinionModule.java
+++ src/main/java/com/inqwise/opinion/common/OpinionModule.java
@@ -3,11 +3,22 @@ package com.inqwise.opinion.common;
 import com.google.inject.AbstractModule;
 
 import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
 
 public abstract class OpinionModule extends AbstractModule {
 	protected final Vertx vertx;
+	private JsonObject config;
 
 	public OpinionModule(Vertx vertx) {
+		this(vertx, vertx.getOrCreateContext().config());
+	}
+	
+	public OpinionModule(Vertx vertx, JsonObject config) {
 		this.vertx = vertx;
+		this.config = config;
+	}
+	
+	public JsonObject config() {
+		return config;
 	}
 }
